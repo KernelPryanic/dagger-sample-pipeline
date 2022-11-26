7a9679929c8e7ea41ab656215d884897dda32ebd
Daniil Trishkin <daniil@kaiko.ai>
2022-11-26 20:42:20 +0100

    Update

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,7 @@ import sys
 
 import anyio
 from dagger import Config, Connection
-from dagger.api.gen import Client, DirectoryID
+from dagger.api.gen import Client, Directory
 
 
 def random_string(n: int) -> str:
@@ -15,23 +15,25 @@ def random_string(n: int) -> str:
     )
 
 
-async def job_a(client: Client):
+async def job_a(client: Client, src_dir: Directory):
     cnt_mnt_dir = f"/{random_string(8)}-src"
 
     await (
         client.container().
-        from_("python:alpine").
-        exec(["python", "-c", "from time import sleep; sleep(5); \"print('Hello world')\""])
+        from_("python:3.11-alpine").
+        with_mounted_directory(cnt_mnt_dir, src_dir).
+        with_workdir(cnt_mnt_dir).
+        exec(["cat", "LICENSE"])
     ).exit_code()
 
 
-async def job_b(client: Client, src_dir_id: DirectoryID):
+async def job_b(client: Client, src_dir: Directory):
     cnt_mnt_dir = f"/{random_string(8)}-src"
 
     await (
         client.container().
-        from_("alpine:latest").
-        with_mounted_directory(cnt_mnt_dir, src_dir_id).
+        from_("ghcr.io/kernelpryanic/dagger-debug:latest").
+        with_mounted_directory(cnt_mnt_dir, src_dir).
         with_workdir(cnt_mnt_dir).
         exec(["cat", "LICENSE"])
     ).exit_code()
@@ -39,11 +41,10 @@ async def job_b(client: Client, src_dir_id: DirectoryID):
 
 async def main():
     async with Connection(Config(log_output=sys.stdout)) as client:
-        src_dir_id = await client.host().workdir().id()
+        src_dir = client.host().workdir()
 
-        async with anyio.create_task_group() as tg:
-            tg.start_soon(job_a, client)
-            tg.start_soon(job_b, client, src_dir_id)
+        await job_a(client, src_dir)
+        await job_b(client, src_dir)
 
 if __name__ == "__main__":
     anyio.run(main)
